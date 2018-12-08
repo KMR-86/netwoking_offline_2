bd268e0cf4d63c9ec6941cd031e96ea1e3ca14d8
mushi1002 <mahmud.mushfique@gmail.com>
2018-12-08 22:01:34 +0600

    first

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo ifconfig enp0s3:1 172.20.56.1 netmask 255.255.255.0 up
+sudo ifconfig enp0s3:2 172.20.56.2 netmask 255.255.255.0 up
+sudo ifconfig enp0s3:3 172.20.56.3 netmask 255.255.255.0 up
+sudo ifconfig enp0s3:4 172.20.56.4 netmask 255.255.255.0 up
+sudo ifconfig enp0s3:100 172.20.56.100 netmask 255.255.255.0 up
